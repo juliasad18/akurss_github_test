cbab911f80023106e764d394786061a4ad6191a2
Julija <julija181292@gmail.com>
2018-04-21 19:32:02 +0300

    test 1

---
+++ src/lv/akurss/github/test/Main.java
@@ -0,0 +1,13 @@
+package lv.akurss.github.test;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Person p = new Person("Alex");
+		
+		System.out.println(p.getName());
+		
+
+	}
+
+}
